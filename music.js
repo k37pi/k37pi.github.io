4e5d37d896cfe5b5f7f4acfab3e07c6564ffa453
k37pi <70585902+k37pi@users.noreply.github.com>
2021-01-14 22:15:25 +0530

    Update music.js

--- music.js
+++ music.js
@@ -88,7 +88,7 @@ function chord()
            var M7 = [chrom[(i)%12],chrom[(i+4)%12],chrom[(i+7)%12],chrom[(i+10)%12]];
            var Maj7 = [chrom[(i)%12],chrom[(i+4)%12],chrom[(i+7)%12],chrom[(i+11)%12]];
            var M9 = [chrom[(i)%12],chrom[(i+4)%12],chrom[(i+7)%12],chrom[(i+2)%12]];
-           j++;  
+           j++;
            break;
           }
       }
@@ -105,5 +105,41 @@ function chord()
      document.querySelector('#M7c').innerHTML = name.charAt(0).toUpperCase() + name.slice(1) +'7 Chord: ' + M7 + '.';
      document.querySelector('#Maj7c').innerHTML = name.charAt(0).toUpperCase() + name.slice(1) +'Maj7 Chord: ' + Maj7 + '.';
      document.querySelector('#M9c').innerHTML = name.charAt(0).toUpperCase() + name.slice(1) +'add9 Chord: ' + M9 + '.';
+    }
+}
+
+function chord1()
+{
+ var chrom = ["A", "A#", "B", "C", "C#", "D", "D#","E","F","F#","G","G#"];
+ let name = document.querySelector('#note1').value;
+ var j = 0
+ for (var i = 0; i < chrom.length; i++)
+     {
+      if (name.charAt(0).toUpperCase() + name.slice(1) === chrom[i])
+         {
+           var m1 = [chrom[(i)%12],chrom[(i+3)%12],chrom[(i+7)%12]];
+           var m71 = [chrom[(i)%12],chrom[(i+3)%12],chrom[(i+7)%12],chrom[(i+10)%12]];
+           var m91 = [chrom[(i)%12],chrom[(i+3)%12],chrom[(i+7)%12],chrom[(i+2)%12]];
+           var M1 = [chrom[(i)%12],chrom[(i+4)%12],chrom[(i+7)%12]];
+           var M71 = [chrom[(i)%12],chrom[(i+4)%12],chrom[(i+7)%12],chrom[(i+10)%12]];
+           var Maj71 = [chrom[(i)%12],chrom[(i+4)%12],chrom[(i+7)%12],chrom[(i+11)%12]];
+           var M91 = [chrom[(i)%12],chrom[(i+4)%12],chrom[(i+7)%12],chrom[(i+2)%12]];
+           j++;
+           break;
+          }
+      }
+ if (name === "" || j === 0)
+    {
+     document.querySelector('#mc1').innerHTML = 'Enter Correct Note';
+    }
+ else
+    {
+     document.querySelector('#mc1').innerHTML = name.charAt(0).toUpperCase() + name.slice(1) +' Minor Chord: ' + m1 + '.';
+     document.querySelector('#m7c1').innerHTML = name.charAt(0).toUpperCase() + name.slice(1) +' Minor7 Chord: ' + m71 + '.';
+     document.querySelector('#m9c1').innerHTML = name.charAt(0).toUpperCase() + name.slice(1) +' madd9 Chord: ' + m91 + '.';
+     document.querySelector('#Mc1').innerHTML = name.charAt(0).toUpperCase() + name.slice(1) +' Major Chord: ' + M1 + '.';
+     document.querySelector('#M7c1').innerHTML = name.charAt(0).toUpperCase() + name.slice(1) +'7 Chord: ' + M71 + '.';
+     document.querySelector('#Maj7c1').innerHTML = name.charAt(0).toUpperCase() + name.slice(1) +'Maj7 Chord: ' + Maj71 + '.';
+     document.querySelector('#M9c1').innerHTML = name.charAt(0).toUpperCase() + name.slice(1) +'add9 Chord: ' + M91 + '.';
     }
 }
