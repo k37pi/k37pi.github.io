5a12f3a52499c18b1e7e4c3306cf45fbecb45640
k37pi <70585902+k37pi@users.noreply.github.com>
2021-08-16 12:55:32 +0530

    Update music.js

--- music.js
+++ music.js
@@ -241,6 +241,6 @@ function modes()
      document.querySelector('#L').innerHTML = name.charAt(0).toUpperCase() + name.slice(1) +' Lydian: ' + Lydian + '';
      document.querySelector('#M').innerHTML = name.charAt(0).toUpperCase() + name.slice(1) +' Mixolydian: ' + Mixolydian + '';
      document.querySelector('#A').innerHTML = name.charAt(0).toUpperCase() + name.slice(1) +' Aeolian: ' + Aeolian + '';
-     document.querySelector('#Lo').innerHTML = name.charAt(0).toUpperCase() + name.slice(1) +' Locrian: ' + Locrian + '';
+     document.querySelector('#Lo').innerHTML = name.charAt(0).toUpperCase() + name.slice(1) +' Locrian: ' + Locrian + ''; 
     }
 }
